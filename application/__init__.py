384f3bcf55e3606277331c4bd832cd93843e01f8
KeysStarks <keishawnstarks@outlook.com>
2026-08-30 07:11:48 -0400

    Add Swagger API Doc

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from .extensions import db, ma, limiter, cache
 from .blueprints.customers import customers_bp
 from .blueprints.mechanics import mechanics_bp
 from .blueprints.service_ticket import service_ticket_bp
 from .blueprints.inventory import inventory_bp
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'app_name': "Mechanic Shop API"}
+)
 
 def create_app(config_class):
     app = Flask(__name__)
@@ -18,5 +28,6 @@ def create_app(config_class):
     app.register_blueprint(mechanics_bp, url_prefix='/mechanics')
     app.register_blueprint(service_ticket_bp, url_prefix='/service-tickets')
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
-
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+    
     return app
